af3ede1524ffa1f7834533e9c1289371d685e567
Naveen J <nvnj1998@gmail.com>
2020-12-08 16:25:22 +0530

    Update and rename ping.sh to bulk-ping.sh

--- bulk-ping.sh
+++ bulk-ping.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Program name: pingall.sh
-echo -e "\033[1;95m-----------------------------------------"
+echo -e "\033[1;95m---------------------------------------------"
 echo "       + The Village Hacker Community +"
-echo -e "  visitme@https://github.com/thevillagehacker"
-echo -e "  Website:https://thevillagehacker.github.io"
-echo -e "----------------------------------------- \033[0m "
-file ="$(1)"
+echo -e " visitme@https://github.com/thevillagehacker"
+echo -e " Website:https://thevillagehacker.github.io"
+echo -e "--------------------------------------------- \033[0m "
+
 #spinner
 spinner ()
 {
-    bar="+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+    bar="+++++++++++++++++++++++++++++++++++++++++++"
     barlength=${#bar}
     i=0
     while ((i <100)); do
@@ -30,9 +30,9 @@ echo "Date: "$now""
 printf "\033[0m"
 
 #calling spinner
-echo -e "\033[1;94m========================================"
-echo     "Initiating Ping request to all Targets"
-echo -e "======================================== \033[0m "
+echo -e "   "
+echo "Initiating Ping request to all Targets"
+echo -e "   "
 spinner
 sleep 0.01
 
@@ -41,12 +41,12 @@ cat $1 |  while read output
 do
     ping -c 1 "$output" > /dev/null
     if [ $? -eq 0 ]; then
-    echo -e "\033[0;92m [+] node [$output] is \033[0;92m up \033[0;92m "
+    echo -e "\033[0;92m  [+] node [$output] is \033[0;92m up \033[0;92m "
     echo -e "------------ \033[0m "
     else
-    echo -e "\033[0;91m [-] node $output is \033[0;91m down \033[0;91m "
+    echo -e "\033[0;91m  [-] node $output is \033[0;91m down \033[0;91m "
     echo -e "------------ \033[0m "
     fi
 done
-echo -e "\033[1;95mGame Over"
+echo -e "\033[1;95mAll Done"
 echo -e "------------------------ \033[0m "
